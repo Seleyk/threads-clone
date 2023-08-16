518fde2250cb0772ad5962df3edd87643b14f320
Steven Seley Jr <seleysteven2@gmail.com>
2023-08-16 08:55:09 -0500

    added thread details and replies

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -35,3 +35,111 @@ export async function createThread({ text, author, communityId, path }: Params) 
         throw new Error(`Could not create thread: ${error.message}`)
     }
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+
+    // Calculate the number of post to skip
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    // Fetch the posts 
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined]}})
+        .sort({ createdAt: 'desc' })
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({ path: 'author', model: User })
+        .populate({
+            path: 'children',
+            populate: {
+                path: 'author',
+                model: User,
+                select: "_id name parentId image"
+            }
+        })
+
+        const totalPostConst = await Thread.countDocuments({
+            parentId: { $in: [null, undefined]}
+        }) // Get the total amount of post
+
+        const posts = await postsQuery.exec();
+
+        const isNext = totalPostConst > skipAmount + posts.length;
+
+        return { posts, isNext };
+}
+
+export async function fetchThreadById(id: string) {
+    connectToDB();
+
+    try {
+        // TODO: populate comunity
+        const thread = await Thread.findById(id)
+            .populate({
+               path: 'author',
+               model: User,
+               select: "_id id name image" 
+            })
+            .populate({
+               path: 'children',
+               populate: [
+                {
+                    path: 'author',
+                    model: User,
+                    select: "_id id name parentId image"
+                },
+                {
+                    path: 'children',
+                    model: Thread,
+                    populate: {
+                        path: 'author',
+                        model: User,
+                        select: "_id id name parentId image"
+                    }
+                }
+               ]
+            }).exec();
+
+            return thread;
+    } catch (error: any) {
+        throw new Error(`Could not retrieve thread: ${error.message}`)
+    }
+    
+}
+
+export async function addCommentToThread(
+    threadId: string,
+    commentText: string,
+    userId: string,
+    path: string,
+) {
+    connectToDB();
+
+    try {
+        // find the original thread by its ID
+        const originalThread = await Thread.findById(threadId);
+
+        if(!originalThread) {
+            throw new Error("Thread not found")
+        }
+
+        // Create a new thread with comment text
+        const commentThread = new Thread({
+            text: commentText,
+            author: userId,
+            parentId: threadId,
+        })
+
+        // save the new thread 
+        const savedCommentThread = await commentThread.save();
+
+        // Update the original thread to include the new comment
+        originalThread.children.push(savedCommentThread._id)
+
+        // save the original thread
+        await originalThread.save();
+
+        revalidatePath(path);
+    } catch (error: any) {
+      throw new Error(`Error adding comment to thread: ${error.message}`)  
+    }
+}
