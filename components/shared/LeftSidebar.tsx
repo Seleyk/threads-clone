c07322470f5ab3d197e672c4f75057f733930f69
Steven Seley Jr <seleysteven2@gmail.com>
2023-08-17 17:42:47 -0500

    added profile page

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { sidebarLinks } from "@/constants";
-import { SignOutButton, SignedIn } from "@clerk/nextjs";
+import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname, useRouter } from 'next/navigation';
+
 
 function LeftSidebar() {
     const router = useRouter();
     const pathname = usePathname();
+    const { userId } = useAuth();
     
     return (
       <section className="custom-scrollbar leftsidebar">
@@ -18,7 +20,7 @@ function LeftSidebar() {
               (pathname.includes(link.route) && link.route.length > 1) ||
               pathname === link.route;
 
-            // if (link.route === "/profile") link.route = `${link.route}/`;
+            if (link.route === "/profile") link.route = `${link.route}/${userId}`;
 
             return (
               <Link
